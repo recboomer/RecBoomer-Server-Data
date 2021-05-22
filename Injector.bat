32c722f00ad3256b3ad8330b2f1f0cf92ee55987
recboomer <82422956+recboomer@users.noreply.github.com>
2021-05-22 20:24:50 +0100

    Add files via upload

---
+++ Injector.bat
@@ -0,0 +1,8 @@
+[
+  {
+    "ManifestString": https://github.com/recboomer/RecBoomerData/ServerData,
+    "FriendlyName": "Injector",
+    "ManifestPatchInfo": {
+      "AssemmblyURL": "https://github.com/recboomer/RecBoomerData/fnjmzsdjehkng",
+      "AssemmblyHash": "3",
+    },
