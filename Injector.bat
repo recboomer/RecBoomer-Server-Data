0bbe0b56dac1e1441888d88d9a2ee41612ec32c5
recboomer <82422956+recboomer@users.noreply.github.com>
2021-05-24 17:52:43 +0100

    Delete Injector.bat

--- Injector.bat
+++
@@ -1,7 +0,0 @@
-[
-  {
-    "ManifestString": https://github.com/recboomer/RecBoomerData/ServerData,
-    "FriendlyName": "Injector",
-    "ManifestPatchInfo": {
-      "AssemmblyURL": "https://github.com/recboomer/RecBoomerData/fnjmzsdjehkng",
-    },
